e9f210e09e1d18203c06260f3e0ea1c1b7e6fe98
Aaron Dandy <aaron.dandy@rezstream.com>
2020-02-10 14:50:49 -0500

    Completes basic XML rewrite functionality

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -1,17 +1,66 @@
-import tl = require('azure-pipelines-task-lib/task');
+import tl = require('azure-pipelines-task-lib');
+import { promises as fsPromises } from 'fs';
+import { DOMParser, XMLSerializer } from 'xmldom';
 
 async function run() {
-    try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-            return;
-        }
-        console.log('Hello', inputString);
+  try {
+    const targetFilePath: string | undefined = tl.getInput('targetFilePath', true);
+    if (targetFilePath === '' || targetFilePath == null) {
+      tl.setResult(tl.TaskResult.Failed, 'A file path to transform is required');
+      return;
     }
-    catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+
+    let fileMutated = false;
+
+    const originalContents = await fsPromises.readFile(targetFilePath, 'utf8');
+    const parser = new DOMParser();
+    
+    let doc = parser.parseFromString(originalContents, 'text/xml');
+    if (doc.documentElement.nodeName !== 'ServiceConfiguration') {
+      tl.setResult(tl.TaskResult.Failed, 'Config file missing root ServiceConfiguration element');
+      return;
     }
+
+    let roleNode = Array.from(doc.documentElement.childNodes).filter(n => n.nodeType === 1 && n.nodeName === 'Role')[0];
+    if (!roleNode) {
+      tl.setResult(tl.TaskResult.Failed, 'Config file missing Role element');
+      return;
+    }
+
+    let configSettingsNode = Array.from(roleNode.childNodes).filter(n => n.nodeType === 1 && n.nodeName === 'ConfigurationSettings')[0];
+    if (!configSettingsNode) {
+      tl.warning("Failed to find a ConfigurationSettings section to update");
+      return;
+    }
+
+    Array.from(configSettingsNode.childNodes).filter(n => n.nodeType === 1 && n.nodeName === 'Setting').forEach(settingsNode => {
+      let settingsElement = <Element>settingsNode;
+      const settingName = settingsElement.getAttribute('name');
+      if (!settingName) {
+        return;
+      }
+
+      let overrideValue = tl.getVariable(settingName);
+
+      if (overrideValue != null) {
+        tl.debug(`Override found for ${settingName}`);
+        settingsElement.setAttribute('value', overrideValue);
+        fileMutated = true;
+      }
+    });
+
+    if (fileMutated) {
+      const newContents = new XMLSerializer().serializeToString(doc);
+      fsPromises.writeFile(targetFilePath, newContents);
+    }
+    else {
+      tl.warning("No variable updates were applied to the file.");
+    }
+
+  }
+  catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
 }
 
 run();
