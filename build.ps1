7002198c7e0493590743ef1bb882339989ee50e9
Aaron Dandy <aaron.dandy@rezstream.com>
2020-03-13 17:39:57 -0400

    Allows the extensions to publish

--- build.ps1
+++ build.ps1
@@ -1,17 +1,29 @@
 Push-Location .\appcfg-settings
-npm install
-tsc
-Pop-Location
+try {
+  npm install
+  tsc
+}
+finally {
+  Pop-Location
+}
 tfx extension create --manifest-globs .\appcfg-settings-extension.json
 
 Push-Location .\cscfg-settings
-npm install
-tsc
-Pop-Location
+try {
+  npm install
+  tsc
+}
+finally {
+  Pop-Location
+}
 tfx extension create --manifest-globs .\cscfg-settings-extension.json
 
 Push-Location .\cscfg-vnetsite
-npm install
-tsc
-Pop-Location
+try {
+  npm install
+  tsc
+}
+finally {
+  Pop-Location
+}
 tfx extension create --manifest-globs .\cscfg-vnetsite-extension.json
